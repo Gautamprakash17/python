8dadc2eb7496bccc88b3617aa21b8abcf104fd81
Gautamprakash17 <gautamprakash17@gmail.com>
2023-07-14 16:07:52 +0530

    1. Two Sum

--- test1.py
+++ test1.py
@@ -1,12 +1,14 @@
-a=[2,7,10,15,45,2,6,3,5,10,12,10,4,9]
-for index in range(len(a)):
-    print("indexes are:",a[index],"=",index)
-for i in range(len(a)):
-    for j in range(i+1,len(a)):
-        #print(a[j],end=' ')
-        if a[i]+a[j]==9:
-           print(a[i],a[j])
-           print("its index is:-",i,j)
+from typing import List
 
-          
-        
+class Solution:
+    @staticmethod
+    def twoSum(nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+nums = [2, 7, 10, 15, 45, 2, 6, 3, 5, 10, 12, 10, 4, 9]
+target = 9
+result = Solution.twoSum(nums, target)
+print("Indexes are:", result)
